5bdbb606407e222a912ef39beb71a867a6426189
Leach <sleach0018@kctcs.edu>
2017-12-24 22:52:43 -0500

    Update

--- app.js
+++ app.js
@@ -11,9 +11,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
